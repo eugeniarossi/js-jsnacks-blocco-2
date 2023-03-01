b2bf819ca0da60b332ed2568c9508238473482e9
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-01 10:58:38 +0100

    snack 1 finito

---
+++ snack1/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let sum = 0;
+/*
+for (let i = 0; i < 5; i++) {
+    const input = Number(prompt('Inserisci un numero'));
+    console.log(input);
+    sum += input;
+}
+ 
+console.log(sum);
+*/
+
+let x = 0;
+while (x < 5) {
+    const input = Number(prompt('Inserisci un numero'));
+    console.log(input);
+    sum += input;
+    x++;
+}
+
+console.log(sum);
